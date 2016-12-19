f6dd52341ccde0ce2e604d7fd799e40851b09a4d
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-19 01:10:41 -0500

    Problems with running the node command, server not starting

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,22 +6,22 @@ var http = require("http");
 var fs = require("fs");
 var portNumber = 3000;
 /**
- * Use fs' object readFileSync() method to
- * asynchronously serve the index.html file
+ * Use fs object' readFileSync() method to
+ * synchronously serve the index.html file
  * The anonymous callback function takes
  * 2 parameters: error and html
  */
-fs.readFileSync('../index.html', function(error, html) {
+fs.readFileSync('./index.html', false, { encoding: "UTF-8" }, function(error, html) {
     if (error) {
         throw error;
     }
     /**
      * http server code
      */
-    var server = http.createServer(function(request, response) {
+    http.createServer(function(request, response) {
         response.writeHeader(200, { 'Content-Type': 'text/html' });
         response.write(html);
         response.end();
     }).listen(portNumber);
-    console.log(`Node server running on: ${portNumber}`); //Backtick characters MIGHT cause a problem
+    console.log("Node server running on:" + portNumber);
 });
--- Scripts/index.js
+++ Scripts/index.js
@@ -3,3 +3,4 @@
  * @file index.js
  * @purpose All the javascript for index.html goes here
  */
+console.info("index.js running fine...");
