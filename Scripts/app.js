08f57a2a5bddffcaa4faa147c6d6ca841f0eb27c
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-19 20:41:53 -0500

    Finished first rebuild of app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,12 @@
 var http = require("http");
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>Hello World!</h1>");
+var fs = require("fs");
+var path = require("path");
+
+http.createServer(function(request, response) {
+    fs.readFile("./index.html", "UTF-8", function(error, html) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(html);
+    });
 }).listen(3000);
 
 console.log("Server running on port 3000");
