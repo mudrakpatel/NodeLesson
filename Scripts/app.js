78ad73262c7ad414ef81f7ec9589e1eacc86e890
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-19 20:13:20 -0500

    Deleted app.js

--- Scripts/app.js
+++
@@ -1,9 +0,0 @@
-/**
-Require statements and other variables
- */
-var express = rquire("express");
-var portNumber = 3000;
-/**
- * Create a new instance of the express application
- */
-var app = express();
