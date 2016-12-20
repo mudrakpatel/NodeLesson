a2802443d161107e44810f3ad8eb3708af3b0934
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-19 19:27:09 -0500

    recoding app.js file again

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,27 +1,9 @@
 /**
-Require statements
+Require statements and other variables
  */
-
-var http = require("http");
-var fs = require("fs");
+var express = rquire("express");
 var portNumber = 3000;
 /**
- * Use fs object' readFileSync() method to
- * synchronously serve the index.html file
- * The anonymous callback function takes
- * 2 parameters: error and html
+ * Create a new instance of the express application
  */
-fs.readFileSync('./index.html', false, { encoding: "UTF-8" }, function(error, html) {
-    if (error) {
-        throw error;
-    }
-    /**
-     * http server code
-     */
-    http.createServer(function(request, response) {
-        response.writeHeader(200, { 'Content-Type': 'text/html' });
-        response.write(html);
-        response.end();
-    }).listen(portNumber);
-    console.log("Node server running on:" + portNumber);
-});
+var app = express();
