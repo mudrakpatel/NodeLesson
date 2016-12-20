e897766a0b95585c1aaceae7d1cb576f8b8e1ba0
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-19 21:03:10 -0500

    Rebuilding app.js needs more attention

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,12 @@
 var http = require("http");
 var fs = require("fs");
 var path = require("path");
+var express = require("express");
+var app = express();
+app.use(express.static("../Content"));
 
 http.createServer(function(request, response) {
-    fs.readFile("./index.html", "UTF-8", function(error, html) {
+    fs.readFile("../index.html", "UTF-8", function(error, html) {
         response.writeHead(200, { "Content-Type": "text/html" });
         response.end(html);
     });
